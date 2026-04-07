e415fd22538f407d2a0aeb2113d0a1c930ffa71e
rayanate <rayanate@mail.uc.edu>
2026-04-06 22:34:55 -0600

    Two sum problem - done

--- src/Week1/Day1/TwoSum.java
+++ src/Week1/Day1/TwoSum.java
@@ -1,8 +1,27 @@
 package Week1.Day1;
 
+import java.util.HashMap;
+
 public class TwoSum {
 
     public static void main(String[] args) {
+
+        int[] nums = new int[] {2,1,3,5,7};
+        int target = 7;
+
+        HashMap<Integer, Integer> twoSumMap = new HashMap<>();
+        int[] result = new int[2];
+        for(int i =0; i< nums.length; i++) {
+            int diff = target-nums[i];
+
+            if(twoSumMap.containsKey(diff)) {
+                result[0] = twoSumMap.get(diff);
+                result[1] = i;
+            }
+            twoSumMap.put(nums[i], i);
+        }
+
+        System.out.println("two sum indices are "+ result[0]+ " and " + result[1]);
 
     }
 }
